60e21d74c2d711e5a14e57159538f2f6758f8d8e
SATYASWARUP DAS <dsatyaswarup@gmail.com>
2024-12-13 13:41:10 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import AboutSection from './components/AboutSection';
-import Testimonials from './components/Testimonials';
-import Footer from './components/Footer';
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
-    <>
+    <div>
+      {/* Navbar */}
       <Navbar />
+
+      {/* Hero Section */}
       <HeroSection />
+
+      {/* About Section */}
       <AboutSection />
+
+      {/* Testimonials Section */}
       <Testimonials />
+
+      {/* Footer */}
       <Footer />
-    </>
+    </div>
   );
 };
 
